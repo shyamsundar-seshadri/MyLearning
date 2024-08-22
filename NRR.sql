ba6e30a36d58dcce89a58742910bea08de0f37c0
Shyam Sundar Seshadri <14096324+shyamsundar-seshadri@users.noreply.github.com>
2024-08-22 22:52:26 +0530

    Update NRR.sql

--- NRR.sql
+++ NRR.sql
@@ -1,21 +1,26 @@
 WITH MatchStats AS (
   SELECT 
-    match_id, 
-    team1, 
-    team1_runs / team1_overs AS team1_run_rate,
-    team2, 
-    team2_runs / team2_overs AS team2_run_rate
-  FROM matches
+    m.match_id, 
+    t1.team_name AS team1, 
+    t1.teamid AS team1_id, 
+    m.team1_runs / m.team1_overs AS team1_run_rate,
+    t2.team_name AS team2, 
+    t2.teamid AS team2_id, 
+    m.team2_runs / m.team2_overs AS team2_run_rate
+  FROM matches m
+  JOIN teams t1 ON m.team1_id = t1.teamid
+  JOIN teams t2 ON m.team2_id = t2.teamid
 ),
 TeamStats AS (
   SELECT
-    team,
-    AVG(CASE WHEN team = team1 THEN team1_run_rate ELSE team2_run_rate END) AS avg_run_rate,
-    AVG(CASE WHEN team = team2 THEN team1_run_rate ELSE team2_run_rate END) AS avg_runs_conceded
+    teamid,
+    team_name,
+    AVG(CASE WHEN teamid = team1_id THEN team1_run_rate ELSE team2_run_rate END) AS avg_run_rate,
+    AVG(CASE WHEN teamid = team2_id THEN team1_run_rate ELSE team2_run_rate END) AS avg_runs_conceded
   FROM MatchStats
-  GROUP BY team
+  GROUP BY teamid, team_name
 )
 SELECT
-  team,
+  team_name,
   avg_run_rate - avg_runs_conceded AS nrr
 FROM TeamStats;
