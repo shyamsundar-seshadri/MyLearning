3ad5fd1990c5933d7587dd1669bab3b42be200a2
Shyam Sundar Seshadri <14096324+shyamsundar-seshadri@users.noreply.github.com>
2024-12-26 21:03:25 +0530

    Update extract-pom-info.sh

--- extract-pom-info.sh
+++ extract-pom-info.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Function to extract groupid, artifactId, and version from a POM file
-extract_pom_info() {
-  local pom_file="$1"
-  local groupid=""
-  local artifactId=""
-  local version=""
+find m2/ -type f -name "*.pom" -exec bash -c '
+    extract_pom_info() {
+        local pom_file="$1"
+        local groupid=""
+        local artifactId=""
+        local version=""
 
-  # Use grep to extract the desired information
-  groupid=$(grep '<groupId>' "$pom_file" | cut -d'>' -f2 | cut -d'<' -f1)
-  artifactId=$(grep '<artifactId>' "$pom_file" | cut -d'>' -f2 | cut -d'<' -f1)
-  version=$(grep '<version>' "$pom_file" | cut -d'>' -f2 | cut -d'<' -f1)
+        groupid=$(grep '<groupId>' "$pom_file" | cut -d'>' -f2 | cut -d'<' -f1)
+        artifactId=$(grep '<artifactId>' "$pom_file" | cut -d'>' -f2 | cut -d'<' -f1)
+        version=$(grep '<version>' "$pom_file" | cut -d'>' -f2 | cut -d'<' -f1)
 
-  echo "$groupid:$artifactId:$version"
-}
-
-# Traverse all subfolders in m2 directory
-find m2/ -type f -name "*.pom" -exec bash -c 'extract_pom_info "$0"' {} \;
+        echo "$groupid:$artifactId:$version"
+    }
+    extract_pom_info "$0"
+' {} \;
